c44029362654e6dc651a4fd39e99a66325a0778e
Carla Fernandez <carlafernandez281@gmail.com>
2025-11-27 15:41:17 -0300

    Guardando trabajo inicial

--- blog-repo/blog/blog/urls.py
+++ blog-repo/blog/blog/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from apps.post.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
 
 if settings.DEBUG:
